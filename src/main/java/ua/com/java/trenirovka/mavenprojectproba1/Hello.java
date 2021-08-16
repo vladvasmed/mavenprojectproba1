8be1721c00f17c91c7f1753f63c3fcdad5b2193f
medvedvv <medvedvv@kho-4229-nb.fuib.com>
2021-08-16 12:02:24 +0300

    Add Operation.java, modify Hello.java

--- src/main/java/ua/com/java/trenirovka/mavenprojectproba1/Hello.java
+++ src/main/java/ua/com/java/trenirovka/mavenprojectproba1/Hello.java
@@ -1,10 +1,12 @@
 package ua.com.java.trenirovka.mavenprojectproba1;
 
 import ua.com.java.trenirovka.mavenprojectproba1.util.PrintStr;
+import ua.com.java.trenirovka.mavenprojectproba1.util.Operation;
 
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, my friend !!!");
 		PrintStr.print("How do you do !??");
+		System.out.println("Summa 5+3 = "+Operation.summa(5, 3));
 	}
 }
---
+++ src/main/java/ua/com/java/trenirovka/mavenprojectproba1/util/Operation.java
@@ -0,0 +1,7 @@
+package ua.com.java.trenirovka.mavenprojectproba1.util;
+
+public class Operation {
+	public static int summa(int x, int y) {
+		return x+y;
+	}
+}
