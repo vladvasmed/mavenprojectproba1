33ba7ce0e5a4a4c8e6598b2816fe97834b404faa
Testov <testov@gmail.com>
2021-08-16 11:25:22 +0300

    modified Hello.java, added PrintStr.java

--- src/main/java/ua/com/java/trenirovka/mavenprojectproba1/Hello.java
+++ src/main/java/ua/com/java/trenirovka/mavenprojectproba1/Hello.java
@@ -1,7 +1,10 @@
 package ua.com.java.trenirovka.mavenprojectproba1;
 
+import ua.com.java.trenirovka.mavenprojectproba1.util.PrintStr;
+
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, my friend !!!");
+		PrintStr.print("How do you do !??");
 	}
 }
---
+++ src/main/java/ua/com/java/trenirovka/mavenprojectproba1/util/PrintStr.java
@@ -0,0 +1,7 @@
+package ua.com.java.trenirovka.mavenprojectproba1.util;
+
+public class PrintStr {
+	public static void print(String paramStr) {
+		System.out.println("Print: "+paramStr);
+	}
+}
